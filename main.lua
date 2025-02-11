8449f49b4adcb9f7b95cbc06054d367f2c2dcb3c
timcbuck <timcbuck.94@gmail.com>
2025-02-11 21:36:10 +1030

    Add STI library and start creating platform objects based on tiled file

--- main.lua
+++ main.lua
@@ -8,8 +8,13 @@ function love.load()
     world = wf.newWorld(0, 1000, false) -- xgravity, ygravity, sleep
     world:setQueryDebugDrawing(debug)
 
-    -- require player object (requires world to be imported first (above))
+    -- Require player object (requires world to be imported first (above))
     require "player"
+
+    -- Initalise tables
+    platforms = {}
+
+    loadLevel("level1.lua")
 end
 
 function love.update(dt)
@@ -19,4 +24,32 @@ end
 
 function love.draw()
     playerDraw()
+end
+
+function loadLevel(level)
+    level = sti("levels/" .. level)
+
+    -- Set player start position
+    for i, obj in pairs(level.layers["Spawn"].objects) do
+        playerStartX = obj.x
+        playerStartY = obj.y
+    end
+    player:setPosition(playerStartX, playerStartY)
+
+    -- Create platforms
+    -- They are polygon shapes so need to loop through all x, y points...
+    for i, obj in pairs(level.layers["Platform"].objects) do
+        for i, pol in pairs(obj.polygon) do
+            print(pol.x)
+        end
+        --createPlatform(obj.x, obj.y, obj.width, obj.height)
+    end
+end
+
+function createPlatform(x, y , width, height)
+    if width > 0 and height > 0 then
+        local platform = world:newRectangleCollider(x, y, width, height, {collision_class = "Platform"})
+        platform:setType("static")
+        table.insert(platforms, platform)
+    end
 end
